7b26e77b2ab7ff9ae640e22bcd2af09051005062
Bloomi <mallowstars@gmail.com>
2026-04-28 22:30:43 +0300

    0.1.0: Button styling and theming (p1)
    Added button of style 1 alongside effects.
    Added basic theming, non functional.

---
+++ bloomi.js
@@ -0,0 +1,25 @@
+/*
+BEHOLD! I am summoning something I shouldn't below.
+*/
+(!async function(){ (
+    void 1,
+    typeof 0,
+    void(new function() {}, 1),
+    delete(new function() {console.log("Loading!");}()),
+    (20,26,4,28, void(!+-void(new(function(/* Project start date! */){})))),
+    typeof(new function(){-~~~~~!!!!!+function(){new(void(function(){/* Do not remove this piece of art. */}))}}),
+    0,1,1,1,0,1,0,1,+0,1,1,1,0,1,1,1,+0,1,1,1,0,1,0,1,
+        +0,0,1,0,0,0,0,0,+0,1,1,0,1,1,1,0,+0,1,1,1,1,0,0,1,+0,1,1,0,0,0,0,1,
+    null,(true,(false,(await(typeof(delete(new function() { ( 1 || (2 / 2^2*4)*30+8-1)})))))),
+    (1)-!2^2%~+(4)-(async function() { /** Future standard for clean code! */
+        let allowFontAwesome = document.documentElement.getAttribute("data-bloomi-import-font-awesome");
+
+        if (allowFontAwesome === "true") {
+            console.log("Bloomi Components has been allowed to import Font Awesome icons, hooray!");
+            let linkFA = document.createElement("link");
+            linkFA.rel = "stylesheet";
+            linkFA.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
+            document.head.appendChild(linkFA);
+            console.log("Bloomi Components has successfully loaded Font Awesome icons!")
+        }
+})())}());
